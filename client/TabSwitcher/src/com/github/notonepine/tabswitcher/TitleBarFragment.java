938e9bfd304943dfc070fcffd09df11d60c85225
vicv <victorvuc@gmail.com>
2014-03-19 01:12:11 -0700

    Added a ToolBarAnimator class to contain all toolbar animations

--- client/TabSwitcher/src/com/github/notonepine/tabswitcher/TitleBarFragment.java
+++ client/TabSwitcher/src/com/github/notonepine/tabswitcher/TitleBarFragment.java
@@ -1,47 +1,46 @@
 package com.github.notonepine.tabswitcher;
 
-import android.animation.ObjectAnimator;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 
 public class TitleBarFragment extends Fragment {
 
-	View mView;
-	View titlebar;
-	View toolbar;
+	private boolean mSwitching = false;
+
+	private View mView;
+	private View toolbar;
+
+	private ToolBarAnimator mToolBarAnimator;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		mView = inflater.inflate(R.layout.title_bar, container, false);
 
-		titlebar = mView.findViewById(R.id.titlebar);
 		toolbar = mView.findViewById(R.id.toolbar);
+
+		mToolBarAnimator = new ToolBarAnimator(
+				mView.findViewById(R.id.titlebar),
+				mView.findViewById(R.id.right_titlebar_buttons),
+				mView.findViewById(R.id.new_tab), getActivity(), mSwitching);
+
 		toolbar.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				animateTitlebar(titlebar.getTranslationX() < 0);
+				mToolBarAnimator.toggleToolbarState();
+				mSwitching = !mSwitching;
 			}
 		});
 
 		return mView;
 	}
 
-	// TODO: move this animation somewhere else
-	private void animateTitlebar(boolean in) {
-		ObjectAnimator anim;
-		if (in) {
-			anim = ObjectAnimator.ofFloat(titlebar, "translationX", 0);
-		} else {
-			float pixels = ViewUtils.dpToPx(getActivity(), -250);
-			anim = ObjectAnimator.ofFloat(titlebar, "translationX", pixels);
-		}
-		anim.setDuration(200);
-		anim.start();
+	public boolean isSwitching() {
+		return mSwitching;
 	}
 
 }
